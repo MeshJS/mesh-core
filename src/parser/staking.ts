81bf95b7a77e56ed21429e3fe5edb699bfabfba5
SIDANWhatever <wongkahinhinson@gmail.com>
2023-12-22 08:21:28 +0900

    fix: renaming staking function

--- src/parser/staking.ts
+++ src/parser/staking.ts
@@ -1,6 +1,6 @@
 import { csl } from '../csl';
 
-export const poolIdHashToBech32 = (poolIdHash: string) => {
+export const poolIdHexToBech32 = (poolIdHash: string) => {
     const cslPoolIdHash = csl.Ed25519KeyHash.from_hex(poolIdHash);
     return cslPoolIdHash.to_bech32('pool');
 };
