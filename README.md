e437a75fba5359dfd0be9c1f81968c663878976c
SIDANWhatever <wongkahinhinson@gmail.com>
2024-03-25 01:28:49 +0800

    feat: release mesh csl

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# sidan-csl
+# mesh-csl
 
-It is a wrapper around [`cardano-serialization-lib`](https://github.com/Emurgo/cardano-serialization-lib) to add user-friendliness and useful APIs for building Cardano off-chain transactions.
+It is a Typescript wrapper around [`sidan-csl-rs`](https://github.com/sidan-lab/sidan-csl-rs) to add user-friendliness and useful APIs for building Cardano off-chain transactions.
---
+++ test/utils/address.test.ts
@@ -0,0 +1,16 @@
+import { serializeBech32Address } from '../../src';
+
+describe('Transaction', () => {
+    test('serializeBech32Address', () => {
+        const address =
+            'addr_test1qqmrzjhtanauj20wg37uk58adyrqfm82a9qr52vdnv0e54r42v0mu8ngky0f5yxmh3wl3z0da2fryk59kavth0u8xhvsufgmc8';
+        const serializedAddress = serializeBech32Address(address);
+        expect(serializedAddress.pubKeyHash).toBe(
+            '36314aebecfbc929ee447dcb50fd690604eceae9403a298d9b1f9a54',
+        );
+        expect(serializedAddress.scriptHash).toBe('');
+        expect(serializedAddress.stakeCredential).toBe(
+            '75531fbe1e68b11e9a10dbbc5df889edea92325a85b758bbbf8735d9',
+        );
+    });
+});
