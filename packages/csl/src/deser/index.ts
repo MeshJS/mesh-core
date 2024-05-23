ab898ffed32afe8aec49bf02b354f17ebc4aa08e
SIDANWhatever <wongkahinhinson@gmail.com>
2024-05-23 12:33:09 +0800

    feat: packaging apply params

--- packages/csl/src/deser/index.ts
+++ packages/csl/src/deser/index.ts
@@ -1,6 +1,4 @@
-export * from './types';
 export * from './deserializer';
 export * from './converter';
 export * from './csl';
 export * from './constants';
-export * from './builder';
--- packages/csl/src/utils/aiken.ts
+++ packages/csl/src/utils/aiken.ts
@@ -1,20 +1,39 @@
-import { csl } from '../deser';
+import { Data } from '@meshsdk/common';
+import { csl, toPlutusData } from '../deser';
 
-export const applyParamsToScript = (rawAikenScriptBlueprint: string, params: string[]): string => {
-    const cslParams = csl.JsVecString.new();
-    params.forEach((param) => {
-        cslParams.add(param);
-    });
-    return csl.apply_params_to_script(cslParams, rawAikenScriptBlueprint);
-};
-
-export const applyObjParamsToScript = <T extends object>(
+export const applyParamsToScript = (
     rawAikenScriptBlueprint: string,
-    params: T[],
+    params: Data[],
+    type: 'Mesh' | 'JSON' | 'CBOR' = 'Mesh',
 ): string => {
     const cslParams = csl.JsVecString.new();
-    params.forEach((param) => {
-        cslParams.add(JSON.stringify(param));
-    });
-    return csl.apply_params_to_script(cslParams, rawAikenScriptBlueprint);
+    let paramType = csl.BuilderDataType.CBOR;
+
+    switch (type) {
+        case 'JSON':
+            paramType = csl.BuilderDataType.JSON;
+            params.forEach((param) => {
+                if (typeof param === 'object') {
+                    cslParams.add(JSON.stringify(param));
+                } else if (typeof param === 'string') {
+                    cslParams.add(param);
+                }
+            });
+            break;
+
+        case 'Mesh':
+            params.forEach((param) => {
+                const paramCbor = toPlutusData(param);
+                cslParams.add(paramCbor.to_hex());
+            });
+            break;
+
+        default:
+            params.forEach((param) => {
+                cslParams.add(param as string);
+            });
+            break;
+    }
+
+    return csl.apply_params_to_script(rawAikenScriptBlueprint, cslParams, paramType);
 };
